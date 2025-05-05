afb415d9f5da27f3ab5ad70643fd5340ca7ef33e
Nanae-j <nanae9577@gmail.com>
2025-05-05 14:06:17 +0900

    [update]社員リストを作成、その中の情報を繰り返し表示

--- test/src/info/seachman/HelloWorld.java
+++ test/src/info/seachman/HelloWorld.java
@@ -1,8 +1,13 @@
 package info.seachman;
 
+import java.util.ArrayList;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
+		//社員リストを作る
+		ArrayList<ShainBean> shainList = new ArrayList<>();
 		
 		//社員１を作る
 		ShainBean shain1 = new ShainBean();
@@ -12,6 +17,8 @@ public class HelloWorld {
 		shain1.setSei("男");
 		shain1.setNen(2003);
 		shain1.setAddress("宮城県仙台市");
+		//社員リストに追加
+		shainList.add(shain1);
 
 		//社員２を作る
 		ShainBean shain2 = new ShainBean();
@@ -21,16 +28,19 @@ public class HelloWorld {
 		shain2.setSei("女");
 		shain2.setNen(2004);
 		shain2.setAddress("福岡県福岡市");
+		//社員リストに追加
+		shainList.add(shain2);
+		
+		// リストの中の社員を表示
+		//社員リストをループする、その中に社員という名前をつける。←ShainBeanが元になっているオブジェクトだよ
+		for (ShainBean shain : shainList) {
+		    System.out.println(shain.getId() + ":" + shain.getName() 
+		        + ":" + shain.getSei() + ":" + shain.getNen() 
+		        + ":" + shain.getAddress());
+		}
+		    
 
-		//表示する
-		System.out.println(shain1.getId()+":"+ shain1.getName()
-		+":"+ shain1.getSei() +":"+ shain1.getNen()
-		+":"+ shain1.getAddress());
 
-		//表示する
-		System.out.println(shain2.getId()+":"+ shain2.getName()
-		+":"+ shain2.getSei() +":"+ shain2.getNen()
-		+":"+ shain2.getAddress());
 	}
 
 }
