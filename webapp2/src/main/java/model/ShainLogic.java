dcae7da59be8470862de788bc818e01f2af5de78
Nanae-j <nanae9577@gmail.com>
2025-05-05 20:49:51 +0900

    [update]社員登録の実装

--- webapp2/src/main/java/model/ShainLogic.java
+++ webapp2/src/main/java/model/ShainLogic.java
@@ -7,10 +7,53 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
 
 import beans.ShainBean;
 
 public class ShainLogic {
+	
+	
+	//リクエストからshainBeanの作成(insert.jspで入力される情報を取得)
+	public ShainBean getShainBean(HttpServletRequest request) {
+		
+		//shainBeanの初期化
+		ShainBean shainBean = new ShainBean();
+		
+		//リクエストから実際のshainBeanの中身のデータを揃えていく
+		//文字列をIntに変換
+		//各input要素のname属性を指定
+		shainBean.setId(Integer.parseInt(request.getParameter("id")));
+		shainBean.setName(request.getParameter("name"));
+		shainBean.setSei(request.getParameter("sei"));
+		shainBean.setNen(Integer.parseInt(request.getParameter("nen")));
+		shainBean.setAddress(request.getParameter("address"));
+		
+		//作成したshainBeanを戻す
+		return shainBean;
+		
+	}
+	
+	//作ったshainBeanのデータをDBに登録
+	public void insertShain(ShainBean shainBean) throws SQLException, NamingException {
+		//社員を登録するsql
+		String sql = "insert into shain(id, name, sei, nen, address) values(?,?,?,?,?);";
+		try (Connection con = ConnectionBase.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);) {
+			//パラメータをSQLにセット
+			pstmt.setInt(1, shainBean.getId());
+			pstmt.setString(2, shainBean.getName());
+			pstmt.setString(3, shainBean.getSei());
+			pstmt.setInt(4, shainBean.getNen());
+			pstmt.setString(5, shainBean.getAddress());
+			//SQl文を表示
+			System.out.println(pstmt.toString());
+			//SQL実行
+			pstmt.executeUpdate();
+		}
+	}
+	
+	
 	//全社員を取得                   //エラーがあった場合にオブジェクト形式でエラーを投げる - このメソッドを使用している元でそのエラーを受け取り処理
 		public ArrayList<ShainBean> getAllShain() throws SQLException, NamingException {
 			
